b79850d9df4dfe8fdced92cf02668b99139c9df8
Munin Borah <muninborah1825@gmail.com>
2026-01-25 18:01:56 +0530

    javascript Day 56

--- learn javascirpt/Day 56/script.js
+++ learn javascirpt/Day 56/script.js
@@ -31,22 +31,4 @@ for (let i = 0; i < givenString.length; i++) {
   }
 }
 console.log(specificString);
-// 23. You are given an array of numbers. Sort the array in **ascending order without using `sort()`**.
-console.log("solution 23");
 
-let num = [3,6,8,4,6,2,4,1,9];
-let currentSmall = num[0];
-let temp = 0;
-let ascendingResult = [temp];
-
-for(let i = 1; i<num.length; i++){
-    let value = num[i];
-
-    if(value < currentSmall){
-        currentSmall = value;
-        ascendingResult.push(currentSmall);
-        temp++;
-    }
-
-}
-console.log(ascendingResult);
